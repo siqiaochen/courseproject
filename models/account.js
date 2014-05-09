943ad3edf6a01ccf8765819edb5a0126b347deb5
siqiaochen <siqiaochen@gmail.com>
2014-05-09 00:59:49 -0700

    add data role

--- models/account.js
+++ models/account.js
@@ -7,9 +7,26 @@ var mongoose = require('mongoose'),
 
 var Account = new Schema({
     nickname: String,
-    birthdate: Date
+    birthdate: Date,
+    roles: String
 });
+var Role = new Schema({
+	created_by : {type: mongoose.Schema.Types.ObjectId, ref: 'Account'},
+    roles: String
+});
+
 
 Account.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('Account', Account);
+
+module.exports = mongoose.model('Role', Role);
+
+exports.requireRole = function(role) {
+    return function(req, res, next) {
+        if(req.session.user && req.session.user.roles === role)
+            next();
+        else
+            res.send(403);
+    }
+}
